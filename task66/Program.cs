73904409f0559bf19035e1413e0dab55814b1eab
Evgenii <joon25@mail.ru>
2023-05-27 14:03:49 +0300

    добавленно решение задачи 66

---
+++ task66/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+SumFromMToN(m, n);
+
+void SumFromMToN(int m, int n)   // вызов функции "сумма чисел от M до N"
+{
+    Console.Write(SumMN(m - 1, n));
+}
+
+int SumMN(int m, int n)   // функция сумма чисел от M до N
+{
+    int res = m;
+    if (m == n)
+        return 0;
+    else
+    {
+        m++;
+        res = m + SumMN(m, n);
+        return res;
+    }
+}
